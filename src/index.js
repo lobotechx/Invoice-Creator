d59d59c970ee0d437877664e82398b158015df1f
lobotechx <lobotechx@gmail.com>
2022-03-16 23:45:26 -0600

    correction2

---
+++ src/index.js
@@ -0,0 +1,23 @@
+btn1 = document.getElementById("btn-1");
+btn2 = document.getElementById("btn-2");
+btn3 = document.getElementById("btn-3");
+btn4 = document.getElementById("btn-4");
+
+textTask = document.getElementById("text-task");
+textWash = document.getElementById("text-Wash");
+textMow = document.getElementById("text-Mow");
+textPull = document.getElementById("text-Pull");
+
+let services = ["Wash Car", "Mow Lawn", "Pull Weeds"];
+
+let prices = {
+  washCar: 10,
+  mowLawn: 20,
+  pullWeeds: 30,
+};
+
+console.log(prices["washCar"]);
+
+btn1.addEventListener("click", function () {
+  for (let i = 0; i < services.length; i++) {}
+});
